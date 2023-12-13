4711d2ad62da34d0317ce7900de44d5324735aef
Tenzing Sherpa <pasangsherpa689@gmail.com>
2023-12-13 11:47:03 +0800

    promises

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const http = require("http");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
 
-const server = http.createServer((req, res) => {
-  console.log("request is received");
-  res.end("hello world");
-});
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-server.listen(5000, () => {});
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise(
+      "./content/result-mind-grenda.txt",
+      "Learning to code backend is quite excitiing",
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// };
+// getText("./content/first.txt")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.log(err));
