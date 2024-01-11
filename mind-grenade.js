5d88f28471e1405a0feadab15f7a4b876adff68d
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-01-11 12:35:14 +0800

    update

--- mind-grenade.js
+++ mind-grenade.js
@@ -2,7 +2,7 @@ const num1 = 44;
 const num2 = 52;
 
 const addValues = (num1, num2) => {
-  console.log(`this is the sum ${num1 + num2}`);
+  console.log(`this is the sum of two numbers provided ${num1 + num2}`);
 };
 
 addValues(num1, num2);
